f0b10555a4de0019fd284a4357a161664755ed9d
Murad Rahmanzada <muradrahmanzada@gmail.com>
2022-09-19 18:57:18 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "./app/store";
+import { addTodo, remove, toggleCompleted } from "./features/todoSlice";
 
 function App() {
+  const todos = useAppSelector((state) => state.todos);
+  const [title, setTitle] = useState("");
+
+  const dispatch = useAppDispatch();
+
+  const onSave = () => {
+    if (title.length > 0) {
+      dispatch(addTodo(title));
+    }
+    setTitle("");
+  };
+
+  const onDelete = (id: string) => {
+    dispatch(remove(id));
+  };
+
+  const toggle = (id: string) => {
+    dispatch(toggleCompleted(id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen flex py-28 justify-center">
+      <div className="border-[3px] border-blue-600 rounded-lg p-10 h-min">
+        <input
+          type="text"
+          className="py-1 px-5 border  border-blue-700 rounded-lg text-xl font-semibold outline-none"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <button
+          className="py-1 px-5 mx-2 text-white text-lg bg-blue-700 rounded-lg "
+          onClick={onSave}
         >
-          Learn React
-        </a>
-      </header>
+          Save
+        </button>
+        {/* TodoList */}
+        {todos.map((todo, index) => (
+          <li key={todo.id} className="py-5 list-none">
+            <span
+              className={`text-xl font-semibold ${
+                todo.completed && "line-through"
+              }`}
+            >
+               {index} - {todo.title}
+            </span>
+            <div className="py-5">
+              <button
+                onClick={() => toggle(todo.id)}
+                className={`bg-green-600 mx-2 rounded-md font-semibold text-white ${todo.completed ? "w-[120px]" : 'w-[100px]'} `}
+              >
+                {todo.completed ? "Not Completed" : "Completed"}
+              </button>
+              <button
+                type="submit"
+                onClick={() => onDelete(todo.id)}
+                className="bg-red-600 mx-2 rounded-md font-semibold text-white w-[100px]"
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
